b2771d0630261458cb29a0c4fdd333cbd938493a
Swadhin Kumar Nayak <114871538+nayakswadhin@users.noreply.github.com>
2023-05-14 20:25:43 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,16 @@
-// document.getElementById('loginForm').addEventListener('submit',(event)=>{
-//     event.preventDefault();
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { auth } from "./firebase.js";
 
-// })
-
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        location.replace("welcome.html")
-    }
-})
-function login(){
+document.getElementById('loginForm').addEventListener('submit',(event)=>{
+    event.preventDefault();
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
+    .then(()=>{
+        console.log("Successfully logged in")
+    })
     .catch((error)=>{
         // document.getElementById("error").innerHTML = error.message
         alert(error);
     })
-}
+})
